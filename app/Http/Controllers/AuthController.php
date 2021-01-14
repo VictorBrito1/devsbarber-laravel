f1d3a00c48d363a04e52db0f9e49a0eb2c996387
Victor Brito <victor.brito@izap.com.br>
2021-01-14 13:49:00 -0300

    Jwt config

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class AuthController extends Controller
+{
+    /**
+     * AuthController constructor.
+     */
+    public function __construct()
+    {
+        $this->middleware('auth:api', ['except' => ['login']]);
+    }
+}
