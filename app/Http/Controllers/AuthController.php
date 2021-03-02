8bb1e7e307f0846deab2c49e2e79036f873a8ff7
Victor Brito <victor_britosoares@hotmail.com>
2021-03-02 14:24:23 -0300

    Authentication

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,6 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+
 class AuthController extends Controller
 {
     /**
@@ -9,6 +13,55 @@ class AuthController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth:api', ['except' => ['login']]);
+        $this->middleware('auth:api', ['except' => ['login', 'unauthorized']]);
+    }
+
+    /**
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function login(Request $request)
+    {
+        $data = $request->only(['email', 'password']);
+
+        Validator::make($data, [
+            'email' => ['string', 'email', 'required'],
+            'password' => ['string', 'min:6']
+        ])->validate();
+
+        $token = Auth()->attempt($data);
+
+        if (!$token) {
+            throw new AccessDeniedHttpException(null, null, 0, ['errors' => 'Bad credentials.']);
+        }
+
+        return response()->json(['token' => $token]);
+    }
+
+    /**
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function refresh()
+    {
+        return response()->json(['token' => auth()->refresh()]);
+    }
+
+    /**
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function logout()
+    {
+        auth()->logout();
+
+        return response()->json(['success' => true]);
+    }
+
+    /**
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function unauthorized()
+    {
+        return response()->json(['errors' => 'Unauthorized'], 401);
     }
 }
